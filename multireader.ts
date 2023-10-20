beca93a4bab6dbdb7bf308aaf3ffe47d3b2833c4
EmilseBilse <71692416+EmilseBilse@users.noreply.github.com>
2023-10-20 15:53:27 +0200

    je

--- multireader.ts
+++ multireader.ts
@@ -21,20 +21,51 @@ fs.readdir(inputDirectory, (err: NodeJS.ErrnoException | null, files: string[]) 
                 return;
             }
 
-            // Transform the data
-            const transformedData: string = data.split('\n').map(line => {
-                if (line.trim() === 'emil') {
-                    return 'john yes';
+            const lines = data.trim().split('\n').filter(line => line !== ""); // This will filter out any empty lines
+            const boardSize = parseInt(lines[0], 10);
+
+            if (isNaN(boardSize) || lines.length < boardSize + 2) {
+                console.error(`File ${file} is not correctly formatted.`);
+                return;
+            }
+
+            // Create the 2D array from the file content
+            const board: string[][] = [];
+            for (let i = 1; i <= boardSize; i++) {
+                board.push([...lines[i]]);
+            }
+
+            const numCoordinates = parseInt(lines[boardSize + 1], 10);
+            const coordinates: [number, number][] = [];
+            for (let i = boardSize + 2; i < boardSize + 2 + numCoordinates; i++) {
+                const [x, y] = lines[i].split(',').map(num => parseInt(num, 10));
+                coordinates.push([x, y]);
+            }
+
+            let lettersArray: string[] = [];
+
+            coordinates.forEach(([x, y]) => {
+                // Adjusting the indices since the coordinates are 1-based
+                const adjustedX = x;
+                const adjustedY = y;
+
+                if (adjustedX < boardSize && adjustedY < boardSize) {
+                    const letter = board[adjustedY][adjustedX];  // Adjusted to get row (y) first and then column (x)
+                    lettersArray.push(letter);
+                } else {
+                    console.error(`Coordinate (${x},${y}) is out of bounds for the board.`);
                 }
-                return line + ' yes';
-            }).join('\n');
+            });
+
+            const formattedString = lettersArray.join('\n');
+            console.log(formattedString);
 
             // Construct the output file name and path
             const outputFileName: string = 'output_' + file;
             const outputFilePath: string = path.join(outputDirectory, outputFileName);
 
             // Write the transformed data to the output file
-            fs.writeFile(outputFilePath, transformedData, (err: NodeJS.ErrnoException | null) => {
+            fs.writeFile(outputFilePath, formattedString, (err: NodeJS.ErrnoException | null) => {
                 if (err) {
                     console.error(`Error writing to the file ${outputFileName}:`, err);
                     return;
